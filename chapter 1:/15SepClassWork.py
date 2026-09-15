b4c92b955018a206abfcf9a9d875481507332031
josh2010-code <22.josh.roberts@msltcastlegregory.ie>
2026-09-15 17:57:39 +0100

    Implement driving licence eligibility and bill splitter
    Added age eligibility checker and split bill calculator.

--- chapter 1:/15SepClassWork.py
+++ chapter 1:/15SepClassWork.py
@@ -22,3 +22,26 @@ if ((username == username1) and (password == password1)) :
     print("correct")
 else:
     print("wrong password or username dweeb!:(")
+print("Welcome to the driving licence eligibilty checker")
+
+age = int(input("what age are you?: "))
+#it is requesting an input from the user as an integer
+name = input("what is ur name?: ")
+#question 16 a iii
+print("you entered",age)
+if ((age >= 17) and (age <=74)) :
+    print("you can apply",name)
+
+elif (age > 74) :
+    print("you can apply for a three year licence",name)
+else :
+    print(name,",you are not eligable")
+    
+
+
+#b
+print("Split bill calculater")
+bill = int(input("Total amount of the bill: "))
+people = int(input("How many people: "))
+owe = bill/people
+print(owe)
