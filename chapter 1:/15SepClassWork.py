3533f708c587c76319b289af66d9e41176720bee
josh2010-code <22.josh.roberts@msltcastlegregory.ie>
2026-09-15 17:19:27 +0100

    Create 15SepClassWork.py

---
+++ chapter 1:/15SepClassWork.py
@@ -0,0 +1,24 @@
+# blackCoffee = 5
+# whiteCoffee = 10
+# print(blackCoffee > whiteCoffee)
+# 
+# blackCoffee = 5
+# whiteCoffee = 10
+# print((blackCoffee == 6) and (whiteCoffee==10))
+# 
+# blackCoffee = 5
+# whiteCoffee = 10
+# print((blackCoffee != 6) or (whiteCoffee==10))
+# 
+# blackCoffee = 5
+# whiteCoffee = 10
+# print(not(blackCoffee > 6) and (whiteCoffee==10))
+
+username = "axl@gunsnroses.com"
+password = "novemberrain"
+username1 = input("enter username: ")
+password1 = input("enter password: ")
+if ((username == username1) and (password == password1)) :
+    print("correct")
+else:
+    print("wrong password or username dweeb!:(")
