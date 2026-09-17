62ed2f3ea3e622cafa79ac4662ce8649c3f2958d
josh2010-code <22.josh.roberts@msltcastlegregory.ie>
2026-09-17 09:01:01 -0100

    Update 17sep.py

--- chapter 1:/17sep.py
+++ chapter 1:/17sep.py
@@ -1,1 +1,18 @@
+# money = 0
+# 
+# while money <=0:
+#     print("You are broke boi🙏 ")
+# 
+# while money >=1:
+#     print("you got $",money)
+cpas = 67
+pas = int(input("INPUT YOUR PASSWORD"))
 
+while pas != cpas:
+    print("wooooooooooooooooooooooooooooooooooooooooooooong password!!!")
+    if pas != cpas:
+        pas = input("try Again")
+        pas =int(pas)
+    
+print("welcome")    
+    
